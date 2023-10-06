e18e46c7b9d3bfc0deceeb21c37e4b3526ad0b21
Deepak Vishwakarma <vishwakarmadeepak310@gmail.com>
2023-10-06 17:31:06 +0000

    todays class code

--- conversionofdatatypes.js
+++ conversionofdatatypes.js
@@ -1,9 +1,33 @@
-let name = "Deepak22445"
+// // let name = "Deepak22445"
 
-let num = Number(name)
-console.log(typeof(name) )
+// // let num = Number(name)
+// // console.log(typeof(name) )
 
-console.log(typeof(num) )
+// // console.log(typeof(num) )
 
-console.log(num)
+// // console.log(num)
 
+// let abc = false
+// // console.log(typeof abc);
+// // console.log(abc);
+
+// let valueInNumber = Number(abc)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//  // "50" >> 50
+//  //"50abc" >> NaN
+
+// // true -> 1; false -> 0
+
+ let x = ""
+ let y = Boolean(x)
+
+ //console.log( y);
+
+
+ let somenumbers = 50
+ let stringnumber = String(somenumbers)
+
+ console.log(typeof stringnumber);
--- datatypes.js
+++ datatypes.js
@@ -2,10 +2,10 @@
 
 //alert(2+5)
 
-let name = "Deepak"
-let age = 25
-let isLoggedIn = false
-let city
+// let name = "Deepak"
+// let age = 25
+// let isLoggedIn = false
+// let city
 
 
 // number 2 to power 53
@@ -17,4 +17,43 @@ let city
 //symbol => unique
 // objects
 
-console.log(typeof undefined)
+//console.log(typeof undefined)
+
+let numbers1 = 5
+let numbers2 = 10
+let someothernumbers = -numbers1
+// console.log(someothernumbers);
+// console.log(numbers1 + numbers2);
+// console.log(numbers1 - numbers2);
+// console.log(numbers1 * numbers2);
+// console.log(numbers1 / numbers2);
+// console.log(numbers1 ** numbers2);
+// console.log(numbers1 % numbers2);
+
+let str1 = "hello"
+let str2 = " coders"
+
+let str3 = str1 + str2
+//console.log(str3);
+let a = "1" + 2
+//console.log(typeof a);
+let b = 1 + "2"
+//console.log(typeof b);
+let c = "1" + 2 + 2
+//console.log(typeof c);
+let d = 1 + 2 + "2"
+//console.log(d);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3 
+num1 = num2 = num3 = 5 + 5
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+let score = 50
+let x = score++;
+console.log(score);
